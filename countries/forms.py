7e877bc9d47495b67c1a1522c1570208b9896025
Sekhar <sekhartsr@gmail.com>
2019-12-01 22:37:05 +0800

    added styles all pages

--- countries/forms.py
+++ countries/forms.py
@@ -14,7 +14,7 @@ class StateForm(forms.ModelForm):
     class Meta:
         model = States
         fields = ['state_name', 'state_capital', 'total_districts']
-        labels = {'state_name': 'State ', 'state_capital': 'Capital: ', 'total_districts': 'No. Districts: '}
+        labels = {'state_name': 'State ', 'state_capital': 'Capital ', 'total_districts': 'No. Districts '}
         widgets = {'state_name': forms.TextInput(attrs={'size': 20, 'title': 'State'}),
                    'state_capital': forms.TextInput(attrs={'size': 20, 'title': 'Capital'}),
                    'total_districts': forms.NumberInput()}
